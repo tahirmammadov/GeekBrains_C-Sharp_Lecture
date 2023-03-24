18326f282bdb6ab6a9b8c500031dc480c7962cf1
Tahir Mammadov <tmammadovt@gmail.com>
2023-03-24 14:01:27 +0400

    add Example005_ConditionIfElse

---
+++ Example005_ConditionIfElse/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+string favorite = "маша";
+
+if (username.ToLower()==favorite){
+    Console.WriteLine("Ура, это же МАША!");
+}else{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
