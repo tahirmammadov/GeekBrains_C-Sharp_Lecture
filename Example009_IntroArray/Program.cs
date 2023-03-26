c3ed8f0f94d64f391c748ee44980737f37d42784
Tahir Mammadov <tmammadovt@gmail.com>
2023-03-26 17:04:41 +0400

    added Example 008, 009, 010, 011

---
+++ Example009_IntroArray/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.Clear();
+
+// int Max(int arg1, int arg2, int arg3)
+// {
+//     int result = arg1;
+//     if (arg2 > result) result = arg2;
+//     if (arg3 > result) result = arg3;
+//     return result;
+// }
+
+// int[] array = {11,211,31,41,15,61,17,18,19};
+
+int Max (int[] array){
+    int i = 0;
+    int max = array[0];
+    while (i<array.Length){
+        if (array[i]>max) max = array[i];
+        i++;
+    } return max;}
+
+int[] massiv = {11,21,31,41,15,61,17,18,19};
+
+Console.WriteLine(Max(massiv));
---
+++ Example011_ArrayLibrary/Program.cs
@@ -0,0 +1,43 @@
+﻿Console.Clear();
+
+void FillArray(int[] collection)
+{
+    for (int i = 0; i < collection.Length; i++)
+    {
+        collection[i] = new Random().Next(0, 10);
+    }
+}
+
+void PrintArray(int[] col)
+{
+    for (int i = 0; i < col.Length; i++)
+    {
+        Console.Write(col[i]+" ");
+    }
+}
+
+int IndexOf(int[] collection, int find)
+{
+    
+    int position = -1;
+    for (int i = 0; i < collection.Length; i++)
+    {
+        if (collection[i] == find)
+        {
+            position = i;
+            break;
+        }
+    }
+    return position;
+}
+
+int[] array = new int[10];
+
+FillArray(array);
+array[4] = 4;
+array[6] = 4;
+
+PrintArray(array);
+Console.WriteLine();
+
+Console.WriteLine(IndexOf(array, 4));
